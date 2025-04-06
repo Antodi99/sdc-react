84a33205943bc1fdc5cd44ad3e87894ba868bb10
Anton Rubchenko <rubchenko.anton@gmail.com>
2025-04-06 15:34:08 +0300

    chore: setup pages component structure

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home/Home"
+export { default as Menu } from "./Menu/Menu"
+export { default as Company } from "./Company/Company"
+export { default as Login } from "./Login/Login"
