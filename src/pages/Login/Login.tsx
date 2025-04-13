d8980c942d3942b5b27a1b2990fc5d68792b0eb3
Anton Rubchenko <rubchenko.anton@gmail.com>
2025-04-13 23:22:54 +0300

    feat: convert pages to a class component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,7 @@
-export default function Login() {
-  return (
-    <div>Login</div>
-  )
+import { Component } from "react"
+
+export default class Login extends Component {
+  render() {
+    return <div>Login</div>
+  }
 }
